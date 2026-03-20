5fc63c54cd265041d2262b216e11eee6a09dac04
movingfrog <s24047@gsm.hs.kr>
2026-03-20 17:55:10 +0900

    feat: 무기 so작성
    #4
    현재 무기에 필요한 것들이 전부 기억나지 않으므로 기본적인 변수만 생성

--- Fantasy-Grower/Assets/Scripts/Equipment/SO_Equipment.cs
+++ Fantasy-Grower/Assets/Scripts/Equipment/SO_Equipment.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SO_Equipment", menuName = "Scriptable Objects/SO_Equipment")]
+[CreateAssetMenu(
+    fileName = "SO_Equipment",
+    menuName = "Scriptable Objects/SO_Equipment",
+    order = 6
+)]
+
 public class SO_Equipment : ScriptableObject
 {
-    
+    public Sprite WeaponImage;
+
 }
