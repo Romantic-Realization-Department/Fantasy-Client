9fd59b23dbc1d9cf939822a7b42fb52ef61aef1e
movingfrog <s24047@gsm.hs.kr>
2026-03-20 17:57:37 +0900

    fix: 모르고 Csharpier를 안했네요..
    #4

--- Fantasy-Grower/Assets/Scripts/Equipment/SO_Equipment.cs
+++ Fantasy-Grower/Assets/Scripts/Equipment/SO_Equipment.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
 
+public enum WeaponLevel
+{
+    C,
+    D,
+    B,
+    A,
+    S,
+}
+
 [CreateAssetMenu(
     fileName = "SO_Equipment",
     menuName = "Scriptable Objects/SO_Equipment",
     order = 6
 )]
-
 public class SO_Equipment : ScriptableObject
 {
+    //필요한 기능에 따라 변수 및 함수가 추가될 수 있음
+    [Header("필요 변수")]
     public Sprite WeaponImage;
 
+    [Header("무기 정보")]
+    public string weaponName;
+    public string weaponInfo;
+    public int Damage;
+    public WeaponLevel currentLevel;
 }
