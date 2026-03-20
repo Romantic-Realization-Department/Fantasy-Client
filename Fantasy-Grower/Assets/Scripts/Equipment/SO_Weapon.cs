9eb143387ba0259979baf4ff0ee131184aedb4f6
movingfrog <s24047@gsm.hs.kr>
2026-03-20 18:02:41 +0900

    fix: CLI 코드 리뷰
    #6

--- Fantasy-Grower/Assets/Scripts/Equipment/SO_Weapon.cs
+++ Fantasy-Grower/Assets/Scripts/Equipment/SO_Weapon.cs
@@ -14,11 +14,11 @@ public enum WeaponLevel
     menuName = "Scriptable Objects/SO_Equipment",
     order = 6
 )]
-public class SO_Equipment : ScriptableObject
+public class SO_Weapon : ScriptableObject
 {
     //필요한 기능에 따라 변수 및 함수가 추가될 수 있음
-    [Header("필요 변수")]
-    public Sprite WeaponImage;
+    [Header("필요 변수"), SerializeField]
+    public Sprite WeaponImage { get; private set; }
 
     [Header("무기 정보")]
     public string weaponName;
