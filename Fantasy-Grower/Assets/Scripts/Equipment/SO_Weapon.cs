e2e0669b1754588b00edde1ea088bec2f0c0f96d
movingfrog <s24047@gsm.hs.kr>
2026-03-23 09:03:30 +0900

    feat: 무기 so에 무기 타입 추가
    #4

--- Fantasy-Grower/Assets/Scripts/Equipment/SO_Weapon.cs
+++ Fantasy-Grower/Assets/Scripts/Equipment/SO_Weapon.cs
@@ -9,11 +9,18 @@ public enum WeaponLevel
     S,
 }
 
-[CreateAssetMenu(
-    fileName = "SO_Equipment",
-    menuName = "Scriptable Objects/SO_Equipment",
-    order = 6
-)]
+public enum WeaponType
+{
+    Rapier,
+    LongSword,
+    GreatSword,
+    GreatBow,
+    CrossBow,
+    Staff,
+    Grimoire,
+}
+
+[CreateAssetMenu(fileName = "SO_Equipment", menuName = "ScriptableObjects/SO_Equipment", order = 6)]
 public class SO_Weapon : ScriptableObject
 {
     //필요한 기능에 따라 변수 및 함수가 추가될 수 있음
@@ -24,5 +31,6 @@ public class SO_Weapon : ScriptableObject
     public string weaponName;
     public string weaponInfo;
     public int Damage;
+    public WeaponType Type;
     public WeaponLevel currentLevel;
 }
