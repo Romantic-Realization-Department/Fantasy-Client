8eb8eeabf0049c43d60c954fbcc7f7981b1899cf
movingfrog <s24047@gsm.hs.kr>
2026-03-23 11:15:20 +0900

    fix: 무기 이미지를 할당 못하던 문제 해결
    #7

--- Fantasy-Grower/Assets/Scripts/Equipment/SO_Weapon.cs
+++ Fantasy-Grower/Assets/Scripts/Equipment/SO_Weapon.cs
@@ -24,8 +24,10 @@ public enum WeaponType
 public class SO_Weapon : ScriptableObject
 {
     //필요한 기능에 따라 변수 및 함수가 추가될 수 있음
-    [Header("필요 변수"), SerializeField]
-    public Sprite WeaponImage { get; private set; }
+    [Header("필요 변수")]
+    [SerializeField]
+    private Sprite WeaponIcon;
+    public Sprite weaponIcon => WeaponIcon;
 
     [Header("무기 정보")]
     public string weaponName;
