857e596d3a4a44a2ffb2dee8412862b95e698f8c
icetea1110 <s24033@gsm.hs.kr>
2026-06-10 09:32:06 +0900

    fix: C# 포맷 수정

--- Fantasy-Grower/Assets/Scripts/Core/SkillTree/Test/TestActiveSkill.cs
+++ Fantasy-Grower/Assets/Scripts/Core/SkillTree/Test/TestActiveSkill.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
 /// <summary>테스트용 액티브 스킬. UseSkill()은 콘솔 출력만 수행한다.</summary>
-[CreateAssetMenu(fileName = "TestActiveSkill", menuName = "ScriptableObjects/SkillTree/Test/ActiveSkill")]
+[CreateAssetMenu(
+    fileName = "TestActiveSkill",
+    menuName = "ScriptableObjects/SkillTree/Test/ActiveSkill"
+)]
 public class TestActiveSkill : ActiveSkillData
 {
     public override void UseSkill()
