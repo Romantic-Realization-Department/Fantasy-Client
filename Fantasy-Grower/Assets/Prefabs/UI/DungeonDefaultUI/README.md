7d49d76c903ccd129433edef81fa79cfdcca4aee
icetea1110 <s24033@gsm.hs.kr>
2026-06-11 20:14:47 +0900

    feat: UI를 이용한 던전 연결

--- Fantasy-Grower/Assets/Prefabs/UI/DungeonDefaultUI/README.md
+++
@@ -1,2 +0,0 @@
-# 요약
-해당 폴더는 Assets/Prefabs/UI/DefaultUI.prefab 의 구성 성분을 프리팹화 시킨 것으로 UI요소의 독립성을 위해 만들어졌습니다.
---
+++ Fantasy-Grower/Assets/Scripts/UI/DungeonSelect/DungeonSelect.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class DungeonSelect : MonoBehaviour
+{
+    [SerializeField]
+    private DungeonSelectButton[] _dungeonSelectButtons;
+
+    private void Awake()
+    {
+        foreach (var d in _dungeonSelectButtons)
+        {
+            d.SelectButton.onClick.AddListener(() => OnClick(d));
+        }
+    }
+
+    private void OnClick(DungeonSelectButton dungeonSelectButton)
+    {
+        if (dungeonSelectButton.Filter.activeSelf)
+        {
+            // 누른 대상만 Select상태로 변경
+            foreach (var d in _dungeonSelectButtons)
+            {
+                if (d == dungeonSelectButton)
+                {
+                    dungeonSelectButton.Select();
+                }
+                else
+                {
+                    d.Return();
+                }
+            }
+        }
+        else
+        {
+            // Select상태로 누르면 씬 이동
+            dungeonSelectButton.MovingScene();
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (_dungeonSelectButtons != null || _dungeonSelectButtons.Length == 0)
+        {
+            Debug.Log("DungeonSelectOnUI가 할당되지 않았습니다!!!", this);
+        }
+    }
+}
