875ee278ab92953d3f71a38f611a4c3d405dbccb
wkdrudgnsdla <s24032@gsm.hs.kr>
2026-07-06 09:23:08 +0900

    feat: Swagger API 엔드포인트 반영

--- Fantasy-Grower/Assets/Scripts/Networks/EndPointSO.cs
+++ Fantasy-Grower/Assets/Scripts/Networks/EndPointSO.cs
@@ -4,45 +4,83 @@ using UnityEngine;
 public class EndPointSO : ScriptableObject
 {
     [field: SerializeField]
-    public string BaseUrl { get; set; } = "http://gsmsv-1.yujun.kr:29859/api/v3/";
+    public string BaseUrl { get; set; } = "https://fantasy.https.gsmsv.site/v1/";
+
+    [Header("Account")]
+    [field: SerializeField]
+    public string AccountEndPoint { get; set; } = "account";
 
     [field: SerializeField]
-    public string AuthEndPoint { get; set; } = "auth/";
+    public string SignUpEndPoint { get; set; } = "account/signup";
+
+    [Header("Auth")]
+    [field: SerializeField]
+    public string LoginEndPoint { get; set; } = "auth/login";
 
     [field: SerializeField]
-    public string LoginEndPoint { get; set; } = "login";
+    public string LogoutEndPoint { get; set; } = "auth/logout";
 
     [field: SerializeField]
-    public string RegisterEndPoint { get; set; } = "register";
+    public string RefreshTokenEndPoint { get; set; } = "auth/refresh";
+
+    [Header("Dungeon")]
+    [field: SerializeField]
+    public string BasicDungeonStateEndPoint { get; set; } = "dungeons/basic/state";
 
     [field: SerializeField]
-    public string LogoutEndPoint { get; set; } = "logout";
-
-    [Header("Player Profile")]
-    [field: SerializeField]
-    public string DataEndPoint { get; set; } = "data/";
+    public string BasicDungeonClaimEndPoint { get; set; } = "dungeons/basic/claim";
 
     [field: SerializeField]
-    public string ProfileEndPoint { get; set; } = "profile/";
+    public string WeaponDungeonEndPoint { get; set; } = "dungeons/weapon";
 
     [field: SerializeField]
-    public string PlayerNameEndPoint { get; set; } = "name";
+    public string BossDungeonEndPoint { get; set; } = "dungeons/boss";
 
+    [field: SerializeField]
+    public string GoldDungeonStartEndPoint { get; set; } = "dungeons/gold-runs";
+
+    [field: SerializeField]
+    public string GoldDungeonClaimEndPoint { get; set; } = "dungeons/gold-runs/{runId}/claim";
+
+    [field: SerializeField]
+    public string GoldDungeonAdRewardEndPoint { get; set; } =
+        "dungeons/gold-tickets/ad-reward";
+
+    [field: SerializeField]
+    public string DungeonTicketsEndPoint { get; set; } = "dungeons/tickets";
+
+    [Header("Game Data")]
+    [field: SerializeField]
+    public string JobSkillsEndPoint { get; set; } = "jobs/{jobType}/skills";
+
+    [field: SerializeField]
+    public string JobWeaponsEndPoint { get; set; } = "jobs/{jobType}/weapons";
+
+    [field: SerializeField]
+    public string LevelsEndPoint { get; set; } = "levels";
+
+    [field: SerializeField]
+    public string StagesEndPoint { get; set; } = "stages";
+
+    [Header("Player")]
     [field: SerializeField]
     public string PlayerEndPoint { get; set; } = "player";
 
     [field: SerializeField]
-    public string PlayerLevelEndPoint { get; set; } = "level";
+    public string PlayerLoadoutEndPoint { get; set; } = "player/loadout";
 
     [field: SerializeField]
-    public string CharacterEndPoint { get; set; } = "character/";
+    public string PlayerSkillUnlockEndPoint { get; set; } = "player/skill/unlock";
+
+    [Header("Tutorial")]
+    [field: SerializeField]
+    public string TutorialsEndPoint { get; set; } = "tutorials";
 
     [field: SerializeField]
-    public string CharacterAllEndPoint { get; set; } = "all";
+    public string TutorialCompleteEndPoint { get; set; } =
+        "tutorials/{tutorialId}/complete";
 
+    [Header("Health")]
     [field: SerializeField]
-    public string Currency { get; set; } = "currency";
-
-    [field: SerializeField]
-    public string Reward { get; set; } = "reward/stage";
+    public string HealthEndPoint { get; set; } = "health";
 }
